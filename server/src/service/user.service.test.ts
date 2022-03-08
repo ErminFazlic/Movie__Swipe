b20c5bd1228ba9d4ec73e874df89d1dc14a6bb2c
jmollyj <jmolly@student.chalmers.se>
2022-03-08 13:32:01 +0100

    Added outline to tests

--- server/src/service/user.service.test.ts
+++ server/src/service/user.service.test.ts
@@ -1,2 +1,34 @@
 ﻿import { addUser } from './user.service';
 
+const register = 
+    
+    
+//User who registered is stored in the database
+test('User registered', () => {
+    afterAll(() => {
+        
+    })
+})
+
+
+//Registered user can log in
+test('User log in', () => {
+    afterAll(() => {
+        
+    })
+})
+
+//User can add friend
+test('User added friend', () => {
+    afterAll(() => {
+
+    })
+})
+
+//User can remove friend
+test('User removed friend', () => {
+    afterAll(() => {
+
+    })
+})
+
