07b5ba053eaf03b7b3d6ff63a9b3d8237a4ffb09
jmollyj <jmolly@student.chalmers.se>
2022-03-10 12:57:19 +0100

    Added Router tests for log in

--- server/src/router/_tests_/router.test.ts
+++ server/src/router/_tests_/router.test.ts
@@ -2,7 +2,8 @@
 import express from "express";
 jest.useFakeTimers()
 
-test("Login User", () => {
+//Log in existing user
+test("Login existing user", () => {
     const req = {
         body: {
             email: "valeria@",
@@ -14,5 +15,21 @@ test("Login User", () => {
     const response = loginUser(req as express.Request, res as express.Response)
     response.then(res=>{
         expect(res.statusCode).toBe(200)
+    }).catch((e : any) => fail(e.message));
+});
+
+//Log in non existing user
+test("Login non-existing user", () => {
+    const req = {
+        body: {
+            email: "test@example.com",
+            password:  "abc"
+        }
+    }
+
+    const res = {};
+    const response = loginUser(req as express.Request, res as express.Response)
+    response.then(res=>{
+        expect(res.statusCode).toBe(500)
     }).catch((e : any) => fail(e.message));
 });
