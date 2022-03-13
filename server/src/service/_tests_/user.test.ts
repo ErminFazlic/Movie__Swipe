e9e1da7853ae4339d64f11ff5700b434f17e8763
jmollyj <jmolly@student.chalmers.se>
2022-03-13 18:19:30 +0100

    moved router tests to user tests

--- server/src/service/_tests_/user.test.ts
+++ server/src/service/_tests_/user.test.ts
@@ -1,4 +1,4 @@
-import {addFriend, addUser} from "../user.service";
+import {addFriend, addUser, loginUser} from "../user.service";
 import express from "express";
 //jest.useFakeTimers()
 
@@ -33,3 +33,35 @@ test("Add friends", () => {
         expect(res.statusCode).toBe(200)
     }).catch((e : any) => fail(e.message));
 });*/
+
+//Log in existing user
+test("Login existing user", () => {
+    const req = {
+        body: {
+            email: "valeria@",
+            password:  "6666"
+        }
+    }
+
+    const res = {};
+    const response = loginUser(req as express.Request, res as express.Response)
+    response.then(res=>{
+        expect(res.statusCode).toBe(200)
+    }).catch((e : any) => fail(e.message));
+});
+
+//Log in non existing user
+test("Login non-existing user", () => {
+    const req = {
+        body: {
+            email: "test@example.com",
+            password:  "abc"
+        }
+    }
+
+    const res = {};
+    const response = loginUser(req as express.Request, res as express.Response)
+    response.then(res=>{
+        expect(res.statusCode).toBe(500)
+    }).catch((e : any) => fail(e.message));
+});
