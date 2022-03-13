927124662e2057c8bea56f076b83118fed58408c
valeria <valeria.nafuna@gmail.com>
2022-03-13 18:36:01 +0100

    intergrationt.est for the user

--- server/src/userintergration.test.ts
+++ server/src/userintergration.test.ts
@@ -1,28 +1,28 @@
 import User from './model/user'
+
 import SuperTest from "supertest";
 import app from "../server"
 import {IUser} from "./types/user.interface"
-jest.useFakeTimers()
 
 const express = require('express') 
- 
 const request : SuperTest.SuperTest<SuperTest.Test> = 
         SuperTest(app);
 
 test("Add a user and login",  async() => {
+    jest.useFakeTimers();
     const usertest : IUser = new User(
         {id:new Date().valueOf(), email:"ermin@gmail.com", password:"9999", username:"ermino", liked:[], disliked:[], friends:[]}
     ); 
-    
+
     request.post('/').
                 send(usertest)
-                .end((err: any, res: any) => {
+                .end((err, res) => {
                     if (err) throw err;
                 });
 
     request.put("/")
-     .then((res) => {
-    expect(res.statusCode).toBe(200);
-    }).catch((e : any) => fail(e.message));
+            .then((res) => {
+            expect(res.statusCode).toBe(200);
+             }).catch((e : any) => fail(e.message));
    
    });
