4e902b9e8f07136497b88b42e4188eab65419cf7
arian <arian.ka.1990@gmail.com>
2022-03-13 13:09:23 +0100

    Intial design for home page.

--- client/frontend/src/components/Home.tsx
+++ client/frontend/src/components/Home.tsx
@@ -2,17 +2,21 @@ import React, { useState } from "react"
 import { Form, Button } from "react-bootstrap";
 import { addFriend } from "../api";
 import Friends from "./Friends";
+import Navigation from "./Navigation";
+import { Container } from "react-bootstrap";
 
 
 export const Home = () => {
     const email: any = localStorage.getItem('email')
-  
+
 
     return (
         <div>
-            <h1>Welcome home {email}</h1>
+            <Navigation />
+            <Container>
+                <Friends />
+            </Container>
 
-            <Friends />
         </div>
     );
 }
